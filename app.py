51f56864e55b06a1ed955291c61389b8bf8b35d0
lebianciotto-web <lebianciotto@gmail.com>
2026-06-26 13:10:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 @st.cache_data
 def load_data():
     # 1. Leer el archivo forzando el separador ;
-    df = pd.read_csv("PR. OMES UNE.csv", sep=';', skiprows=9)
+    df = pd.read_csv("PR. OMES UNE.csv", sep=';', skiprows=7)
     
     # 2. LIMPIEZA CRÍTICA: Eliminar espacios en blanco de los nombres de columnas
     df.columns = df.columns.str.strip()
